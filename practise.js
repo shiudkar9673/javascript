57043b76c890a46b7d9eafa296d0d63819940f13
Mangesh Shiudkar <shiudkar9673@gmail.com>
2025-07-03 19:30:59 +0530

    changes made

--- practise.js
+++ practise.js
@@ -1,5 +1,18 @@
 console.log("mangehs")
 
-const age='23'
+let age='23'
 
 console.log(typeof age)
+
+const b=true;
+console.log(typeof age)
+age=Number(age);
+
+let n=24
+console.log(typeof n)
+n=String(n)
+console.log(typeof n)
+console.log(typeof age)
+
+console.log("1">1)
+console.log(2===2)
